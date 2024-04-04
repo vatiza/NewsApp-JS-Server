7f4c376758e157bab36b414b853ce7144423c422
vatiza <rakibur.mac@gmail.com>
2024-04-04 13:47:01 +0600

    News App Express JS Server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app=express();
+const port=process.env.PORT || 5000;
+var cors = require('cors')
+const categories=require('./data/categories.json');
+
+
+
+app.use(cors())
+app.get('/',(req,res)=>{
+	res.send('News App is Running');
+});
+
+app.get('/categories',(req,res)=>{
+	res.send(categories);
+})
+app.listen(port,()=>{
+	console.log(`News API Is Running on  Port:${port}`);
+})
