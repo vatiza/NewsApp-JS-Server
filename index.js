5b73a2dc87e5fadcceb69a4e0a9a8599d3c4c1a9
vatiza <rakibur.mac@gmail.com>
2024-04-06 02:38:51 +0600

    news data load

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express();
 const port=process.env.PORT || 5000;
 var cors = require('cors')
 const categories=require('./data/categories.json');
-
+const news=require('./data/news.json');
 
 
 app.use(cors())
@@ -12,8 +12,15 @@ app.get('/',(req,res)=>{
 });
 
 app.get('/categories',(req,res)=>{
-	res.send(categories);
+	res.send(categories);	
+	
+});
+
+app.get('/news',(req,res)=>{
+	res.send(news)
 })
+
+
 app.listen(port,()=>{
 	console.log(`News API Is Running on  Port:${port}`);
 })
