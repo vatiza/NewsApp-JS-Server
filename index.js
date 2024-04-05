050cf2e672026d0ba3e2f76a3000c85414f41de0
vatiza <rakibur.mac@gmail.com>
2024-04-06 03:14:03 +0600

    server send data with id

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ app.get('/categories',(req,res)=>{
 
 app.get('/news',(req,res)=>{
 	res.send(news)
+});
+app.get('/news/:id',(req,res)=>{
+	const id=req.params.id;
+
+	const selectedNews=news.find(n=>n._id===id);
+	res.send(selectedNews);
+});
+
+app.get('/categories/:id',(req,res)=>{
+	const id=req.params.id;
+	if(id==0){
+		res.send(news);
+	}else{
+		const categoryNews=news.filter(n=>n.category_id===id);
+	res.send(categoryNews);
+	}
+	
 })
 
 
